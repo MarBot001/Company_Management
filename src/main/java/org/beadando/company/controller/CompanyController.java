5bb8a75c71296c0ed790f7f596d0d1090f8d9585
Marbot001 <droidhun01@gmail.com>
2025-05-01 18:09:22 +0200

    2. commit - Frontend felépítése, vázszerkezet kialakítása.

--- src/main/java/org/beadando/company/controller/CompanyController.java
+++ src/main/java/org/beadando/company/controller/CompanyController.java
@@ -3,11 +3,13 @@ package org.beadando.company.controller;
 import org.beadando.company.model.Company;
 import org.beadando.company.service.CompanyService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@RestController
+@Controller
 @RequestMapping("/companies")
 public class CompanyController {
 
@@ -19,21 +21,38 @@ public class CompanyController {
     }
 
     @GetMapping
+    @ResponseBody // csak ez maradhat REST-es
     public List<Company> getAll() {
         return companyService.findAll();
     }
 
+    // 👇 HTML nézet visszaadása
+    @GetMapping("/view")
+    public String showCompanies(Model model) {
+        model.addAttribute("companies", companyService.findAll());
+        return "companies";
+    }
+
+    @GetMapping("/new")
+    public String showAddForm(Model model) {
+        model.addAttribute("company", new Company());
+        return "new-company";
+    }
+
     @PostMapping
-    public Company add(@RequestBody Company company) {
-        return companyService.add(company);
+    public String saveCompany(@ModelAttribute Company company) {
+        companyService.add(company);
+        return "redirect:/companies/view";
     }
 
     @GetMapping("/{id}")
+    @ResponseBody
     public Company getById(@PathVariable String id) {
         return companyService.findById(id);
     }
 
     @DeleteMapping("/{id}")
+    @ResponseBody
     public boolean delete(@PathVariable String id) {
         return companyService.delete(id);
     }
